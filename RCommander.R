4d3593b91ea898271fe03ac4ae66c78def868b37
Christopher Terry <60357823+AlenTopher@users.noreply.github.com>
2021-08-23 18:07:17 -0400

    Add files via upload

---
+++ RCommander.R
@@ -0,0 +1,13 @@
+#Author: Christopher, Date: Aug 17 2021, Purpose: Calculate stdev, Median values
+
+#variable for number values
+variable1 <- c(1, 2, 3, 4, 5, 6, 7)
+variable1
+
+#standard Deviation
+sd(variable1)
+#2.160247
+
+#Calculate Median
+median(variable1)
+#4
---
+++ RscriptModeCalulate.R
@@ -0,0 +1,10 @@
+#Author: Christopher Terry, Aug 20 2021, 
+#variable1
+variable1 <- c(19,4,5,7,29,19,29,13,23,19)
+
+#calcuate mode
+
+library("modeest")
+
+mlv(variable1, method = "mfv")
+
