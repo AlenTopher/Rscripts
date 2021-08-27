379646fbdfb3ecec56b45395f9f4fbabac4de507
Christopher Terry <60357823+AlenTopher@users.noreply.github.com>
2021-08-27 15:24:04 -0400

    Add files via upload

---
+++ RAug23Install.R
@@ -0,0 +1,14 @@
+# Author: Christopher Terry, Date: Aug 23, 2021, Purpose: Calculate the pearson correlation coefficient value
+
+# Install a package called "ggpubr"
+
+if(!require(devtools)) install.packages("devtools") 
+devtools::install_github("kassambara/ggpubr")
+
+# Load library ggpubr
+
+library("ggpubr")
+
+# Load some sample dataset
+setwd("/run/media/dante/88C5-33A4/Rscripts")
+
